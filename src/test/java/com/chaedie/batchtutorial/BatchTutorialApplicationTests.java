09485a6751d3a532223cffc3d778df00601f1dd4
ChaedongIm <chaedong.im.dev@gmail.com>
2025-06-08 22:59:20 +0900

    initial commit

---
+++ src/test/java/com/chaedie/batchtutorial/BatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaedie.batchtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
